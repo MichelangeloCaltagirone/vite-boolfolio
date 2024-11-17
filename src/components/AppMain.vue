a5c9a2ec5e4fd0344821d206ebac0bfc8e63c05f
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-17 11:44:18 +0100

    Connected the right EndPoint for the exposure of the Api.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,19 +5,33 @@ export default {
     name: "AppMain",
     data() {
         return {
-
+            projectList: [],
         }
     },
     methods: {
         getProjects(){
-            
+            axios.get('/user?ID=12345')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+            }
         }
-    }
 }
 
 </script>
 
 <template>
+
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
 
 </template>
 
