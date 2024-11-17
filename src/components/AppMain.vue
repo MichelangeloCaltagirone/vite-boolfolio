5a249d61bfa4e348a1373850b32d1c54c71fb3b5
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-17 11:23:25 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getProjects(){
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
