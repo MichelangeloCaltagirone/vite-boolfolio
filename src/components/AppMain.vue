f7cfa9b9cced4065577298a4123fe21757de9d9a
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-18 17:26:09 +0100

    Add in the Main component the visualization of all project asnwered by the Api call.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,9 +11,10 @@ export default {
     methods: {
         getProjects(){
             axios.get('http://127.0.0.1:8000/api/projects')
-                .then(function (response) {
+                .then((response) => {
                     // handle success
-                    console.log(response);
+                    console.log(response.data.result);
+                    this.projectList = response.data.result;
                 })
                 .catch(function (error) {
                     // handle error
@@ -31,9 +32,21 @@ export default {
 <template>
 
     <main>
+
         <div class="container">
-            <h1>Ciao, Funziono</h1>
+            <h1>Projects List</h1>
+
+            <div class="my-1" v-for="project in projectList" :key="project.id">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title"><strong>Project Name:</strong> {{ project.name }}</h3>
+                        <h5 class="card-subtitle"><strong>Project Author:</strong> {{ project.author }}</h5>
+                        <p class="card-text mt-1"><strong>Description</strong><br> {{ project.description }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </main>
 
 </template>
